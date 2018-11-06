d03dd771e6fd9407b33649d70e4f80fda5276dc4
James Rodney <jlrodney95@gmail.com>
2018-11-06 15:24:32 +0000

    Added tests for withdraw and deposit
    - They are not private so needed to be tested.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,23 +4,27 @@ require 'date'
 RSpec.describe Account do
   let(:mockPrinter) { double :printer, string_converter: nil }
 
-  # describe('Balance methods') do
-  #   it('should be able to update balance after a withdrawal') do
-  #     new_acc = Account.new
-  #     new_acc.withdraw(50)
-  #     expect(new_acc.balance).to eq(-50)
-  #   end
-  #   it('should be able to update balance after a deposit') do
-  #     new_acc = Account.new
-  #     new_acc.deposit(50)
-  #     expect(new_acc.balance).to eq(50)
-  #   end
-  # end
+  before(:each) do
+    allow(Time).to receive(:now).and_return('time')
+  end
+
+  describe('Balance methods') do
+    it('should be able to update balance after a withdrawal') do
+      acc = Account.new
+      acc.deposit(323.56)
+      expect(acc.withdraw(50.93)).to eq([
+        ['time', nil, "50.93", "272.63"],
+        ['time', "323.56", nil, "323.56"]])
+    end
+    it('should be able to update balance after a deposit') do
+      new_acc = Account.new
+      expect(new_acc.deposit(50)).to eq([["time", "50.00", nil, "50.00"]])
+    end
+  end
 
   describe('statement') do
     it('should convert the account history into a statement form') do
       acc = Account.new(0, [], mockPrinter)
-      allow(Time).to receive(:now).and_return('time')
       acc.deposit(50)
       acc.deposit(40)
       acc.withdraw(50)
